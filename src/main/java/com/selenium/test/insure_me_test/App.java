f6aec6311e480eb87c0ecf051e767d603fa7571b
Neha <nehamandar@gmail.com>
2025-03-17 00:28:28 -0500

    insure-me-test demo application configured

---
+++ src/main/java/com/selenium/test/insure_me_test/App.java
@@ -0,0 +1,57 @@
+package com.selenium.test.insure_me_test;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.time.Duration;
+
+
+public class App {
+    public static void main(String[] args) {
+    	    	   	  
+    	  ChromeOptions options= new ChromeOptions();
+    	  options.addArguments("--headless");
+          
+       	  WebDriver driver = new ChromeDriver(options);
+
+          driver.get("http://52.207.214.202:8081/contact.html");
+
+          System.out.println(driver.getTitle());
+          
+          driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));          
+      
+          WebElement name = driver.findElement(By.id("inputName"));
+          name.sendKeys("Neha Thakur");
+          
+          WebElement number = driver.findElement(By.id("inputNumber"));
+          number.sendKeys("999999999");
+          
+          WebElement email = driver.findElement(By.id("inputMail"));
+          email.sendKeys("nehamandar@xyz.com");
+          
+          WebElement message = driver.findElement(By.id("inputMessage"));
+          message.sendKeys("Hello, How are you");   
+          
+          WebElement submitButton = driver.findElement(By.id("my-button"));
+          submitButton.click();
+          
+          WebElement response = driver.findElement(By.id("response"));
+          response.getText();
+
+                   
+          if (response.getText().equalsIgnoreCase("Message Sent"))
+          {
+        	  System.out.println("Test case Passed");
+          }
+          else 
+          {
+        	  System.out.println("Test case Failed");
+          }
+          
+          driver.quit();
+         
+       }    	    	
+  }
